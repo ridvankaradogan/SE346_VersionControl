7714bbd67874135ead176f7adf413d43643e973f
ridvankaradogan <97835750+ridvankaradogan@users.noreply.github.com>
2022-05-13 16:26:00 +0300

    Add files via upload
    v1

---
+++ SE346 Lab_GitHub.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("SE346 Version Control:\n");
+	
+	//please write your name,student id and department by using printf() function
+
+	printf("Rıdvan Karadogan,18243610045,ISE\n");
+
+
+}
